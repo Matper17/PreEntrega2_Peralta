4dd82e061094d15d542299e2ba65f1ff0874a96a
Matper17 <matiasperalta992@gmail.com>
2023-09-03 21:39:34 -0300

    Ultimos cambios

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -20,7 +20,7 @@ const ItemDetail = ( {item} ) => {
   return (
     <div className="container">
         <div className="producto-detalle">
-            <img src={item.imagen} alt=""/>
+        <img src={item.imagen} alt={item.marca}/>
             <div>
                 <h3 className="titulo">{item.marca}</h3>
                 <p className="descripcion">{item.modelo}</p>
